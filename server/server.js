3d2266cad953d134d30ea80306ca8b26dfacc595
Brandon Wong <b.lh.wong@gmail.com>
2017-03-10 11:44:52 -0800

    Add generateRandomString function for random string stored in cookie

--- server/server.js
+++ server/server.js
@@ -7,19 +7,25 @@ app.set('port', 3000);
 
 app.use(express.static(__dirname + '/../client'));
 
-app.get('/login', function(req, res) {
-
-});
+var generateRandomString = function(length) {
+  var string = '';
+  var possible = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+  for (var i = 0; i < length; i++) {
+    var random = Math.floor(Math.random() * possible.length);
+    string += possible.charAt(random);
+  }
+  return string;
+};
 
 var stateKey = 'spotify_auth_state';
 
 app.get('/login', function(req, res) {
-
+  var state = generateRandomString(16);
 });
 
-app.get('/home', function(req, res)) {
+app.get('/home', function(req, res) {
   res.send('Logged in!');
-};
+});
 
 if (!module.parent) {
   app.listen(app.get('port'));
