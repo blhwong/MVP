80d7ba8ec2ad9faa24497d038f3012af910d66d6
Brandon Wong <b.lh.wong@gmail.com>
2017-03-09 19:26:02 -0800

    Server running with express

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function(req, res) {
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
 
+app.listen(3000, function() {
+  console.log('Listening on port 3000!');
 });
