547d848cd61cf9dbbbbd1431b47582f9d7c5e4b2
Brandon Wong <b.lh.wong@gmail.com>
2017-03-10 11:31:16 -0800

    Add query-string module and create end points for login and home

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 var express = require('express');
 var app = express();
 var config = require('../config/config');
+var queryString = require('query-string');
 
 app.set('port', 3000);
 
 app.use(express.static(__dirname + '/../client'));
+
+app.get('/login', function(req, res) {
+
+});
+
+var stateKey = 'spotify_auth_state';
+
+app.get('/login', function(req, res) {
+
+});
+
+app.get('/home', function(req, res)) {
+  res.send('Logged in!');
+};
 
 if (!module.parent) {
   app.listen(app.get('port'));
