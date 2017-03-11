3f740cdef222c0e453133ee47d74d16229314a32
Brandon Wong <b.lh.wong@gmail.com>
2017-03-10 21:13:42 -0800

    Change ghetto onClick to anchor ref element

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -4,14 +4,8 @@ import $ from 'jquery';
 
 class App extends React.Component {
 
-
-  onClick() {
-    console.log(window);
-    window.location = '/login';
-  }
-
   render () {
-    return <div onClick={this.onClick}> Get Started Here! </div>;
+    return <a href='/login'> Get Started Here! </a>;
   }
 }
 
