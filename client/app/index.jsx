bfa08e9471e813fce223c24e97f25a383ff49e1e
Brandon Wong <b.lh.wong@gmail.com>
2017-03-11 21:10:15 -0800

    Does not callSpotify multiple times when listData is not empty

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -8,34 +8,37 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      listData: []
+      listData: undefined
     };
   }
 
   callSpotify() {
-    var access_token = window.location.hash.split('=');
-    access_token = access_token[1].split('&refresh_token');
-    access_token = access_token[0];
-    // console.log('Calling Spotify');
-    // console.log(access_token);
-    $.ajax({
-      url: 'https://api.spotify.com/v1/me/player/recently-played',
-      type: 'GET',
-      contentType: 'application/json',
-      headers: {
-        Authorization: 'Bearer ' + access_token
-      },
-      data: {
-        limit: 50
-      },
-      success: function(data) {
-        console.log('success!');
-        console.log(data);
-      },
-      error: function() {
-        console.log('fail');
-      }
-    });
+    console.log('hello', !this.state.listData);
+    if (!this.state.listData) {
+      console.log('calling spotify');
+      var app = this;
+      var access_token = window.location.hash.split('=');
+      access_token = access_token[1].split('&refresh_token');
+      access_token = access_token[0];
+      $.ajax({
+        url: 'https://api.spotify.com/v1/me/player/recently-played',
+        type: 'GET',
+        contentType: 'application/json',
+        headers: {
+          Authorization: 'Bearer ' + access_token
+        },
+        data: {
+          limit: 50
+        },
+        success: function(data) {
+          console.log('success!');
+          app.setState({listData: data});
+        },
+        error: function() {
+          console.log('fail');
+        }
+      });
+    }
   }
   render () {
     return (
