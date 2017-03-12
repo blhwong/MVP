8d8abff1a593ed18b913a6ff8ad8688eb6a56916
Brandon Wong <b.lh.wong@gmail.com>
2017-03-12 14:13:41 -0700

    Add functionality for different options in ajax calls

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -10,32 +10,63 @@ class App extends React.Component {
     this.state = {
       listData: undefined,
       option: '',
-      username: undefined
+      username: undefined,
+      access_token: undefined
     };
   }
 
-  callSpotify() {
+  callSpotify(option = 'Your most recently played:') {
     console.log('hello', !this.state.listData);
-    if (!this.state.listData) {
+
+    if (!this.state.listData || this.state.option !== option) {
       console.log('calling spotify');
       var app = this;
       var access_token = window.location.hash.split('=');
       access_token = access_token[1].split('&refresh_token');
       access_token = access_token[0];
+      var url;
+      var data = {
+        limit: 50
+      };
+      if (option === 'Your Top Artists (Over several years):') {
+        url = 'https://api.spotify.com/v1/me/top/artists';
+        data.time_range = 'long_term';
+      } else if (option === 'Your Top Tracks (Over several years):') {
+        url = 'https://api.spotify.com/v1/me/top/tracks';
+        data.time_range = 'long_term';
+      } else if (option === 'Your Top Artists (Over last 6 months):') {
+        url = 'https://api.spotify.com/v1/me/top/artists';
+        data.time_range = 'medium_term';
+      } else if (option === 'Your Top Tracks (Over last 6 months):') {
+        url = 'https://api.spotify.com/v1/me/top/tracks';
+        data.time_range = 'medium_term';
+      } else if (option === 'Your Top Artists (Over last 4 weeks):') {
+        url = 'https://api.spotify.com/v1/me/top/artists';
+        data.time_range = 'short_term';
+      } else if (option === 'Your Top Tracks (Over last 4 weeks):') {
+        url = 'https://api.spotify.com/v1/me/top/tracks';
+        data.time_range = 'short_term';
+      } else {
+        url = 'https://api.spotify.com/v1/me/player/recently-played';
+      }
+
       $.ajax({
-        url: 'https://api.spotify.com/v1/me/player/recently-played',
+        url: url,
         type: 'GET',
         contentType: 'application/json',
         headers: {
           Authorization: 'Bearer ' + access_token
         },
-        data: {
-          limit: 50
-        },
+        data: data,
         success: function(data) {
           console.log('success!');
           console.log(data);
           app.setState({listData: data.items});
+          if (!app.state.access_token) {
+            console.log('state', app.state)
+            // app.setState({access_token: access_token});
+          }
+          app.setState({option: option});
           // do i have username?
             // no call endpoint to receive current user profile and create profile in db
           // call another endpoint to insert info to db from either username in state
@@ -49,15 +80,31 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <a href='/login'>{window.location.hash.length > 0 && this.callSpotify()}Login</a>
-        <a></a>
-        <a></a>
-        <a></a>
-        <a></a>
-        <a></a>
-        <a></a>
-        <input type="button"></input>
-        <h2>Your most recent tracks:</h2>
+        <div>
+          <a href='/login'>{window.location.hash.length > 0 && this.callSpotify()}Login</a>
+        </div>
+        <div>
+          <a href='/' onclick="return false">{this.state.access_token && this.callSpotify('Your most recently played:')}Your most recently played:</a>
+        </div>
+        <div>
+        <a href='/' onclick="return false">{this.state.access_token && this.callSpotify('Your Top Artists (Over several years):')}Your Top Artists (Over several years):</a>
+        </div>
+        <div>
+          <a href='/'>{this.state.access_token && this.callSpotify('Your Top Tracks (Over several years):')}Your Top Tracks (Over several years):</a>
+        </div>
+        <div>
+          <a href='/'>{this.state.access_token && this.callSpotify('Your Top Artists (Over last 6 months):')}Your Top Artists (Over last 6 months):</a>
+        </div>
+        <div>
+          <a href='/'>{this.state.access_token && this.callSpotify('Your Top Tracks (Over last 6 months):')}Your Top Tracks (Over last 6 months):</a>
+        </div>
+        <div>
+          <a href='/'>{this.state.access_token && this.callSpotify('Your Top Artists (Over last 4 weeks):')}Your Top Artists (Over last 4 weeks):</a>
+        </div>
+        <div>
+          <a href='/'>{this.state.access_token && this.callSpotify('Your Top Tracks (Over last 4 weeks):')}Your Top Tracks (Over last 4 weeks):</a>
+        </div>
+        <h2>{this.state.option}</h2>
         <div>
           <List list={this.state.listData}/>
         </div>
